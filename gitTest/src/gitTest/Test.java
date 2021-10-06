7324f2262254be57de7376d5ca3f1e4f47547678
mulmule <92025822+mulmule@users.noreply.github.com>
2021-10-06 22:51:32 +0900

    gitTest

---
+++ gitTest/src/gitTest/Test.java
@@ -0,0 +1,16 @@
+package gitTest;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+	    final LocalDateTime now = LocalDateTime.now();
+
+	    System.out.println("Hello, World! The current time is " + dtf.format(now));
+	}
+
+}
