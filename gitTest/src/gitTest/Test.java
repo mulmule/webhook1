af721f838b9a5d5eecfc7d95bd3d91c2a57310a7
mulmule <92025822+mulmule@users.noreply.github.com>
2021-10-08 12:43:03 +0900

    1243

--- gitTest/src/gitTest/Test.java
+++ gitTest/src/gitTest/Test.java
@@ -10,7 +10,7 @@ public class Test {
 		final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
 	    final LocalDateTime now = LocalDateTime.now();
 
-	    System.out.println("Hello, World!?? The current time is " + dtf.format(now));
+	    System.out.println("Hello, World!?!? The current time is " + dtf.format(now));
 	}
 
 }
